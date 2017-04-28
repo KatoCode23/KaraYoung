c8106f94691ebae697f3380cb808414bff1dfb17
KatoCode23 <Kalen23Imani@gmail.com>
2017-04-28 09:58:00 -0400

    Index.html starting to take shape

---
+++ app.js
@@ -0,0 +1,112 @@
+var app = angular.module('app', []);
+
+app.controller('myController', function($scope){
+
+var log = console.log;
+
+log('Hello World!!!!')
+
+
+$scope.karasPictures = [
+   {
+      name: 'Home Image',
+      link: 'http://www.vogue.com/',
+      img1: '10.jpg',
+      img2: null,
+      img3: null
+
+   },
+   {
+      name: 'Image 1',
+      link: 'http://www.vogue.com/',
+      img1: '11.jpg',
+      img2: null,
+      img3: null
+
+   },
+   {
+      name: 'Image 2',
+      link: 'http://www.vogue.com/',
+      img1: '12.jpg',
+      img2: null,
+      img3: null
+   },
+   {
+      name: 'Image 3',
+      link: 'http://www.vogue.com/',
+      img1: '9.jpg',
+      img2: null,
+      img3: null
+
+   },
+   {
+      name: 'Image 4',
+      link: 'http://www.vogue.com/',
+      img1: '8.jpg',
+      img2: null,
+      img3: null
+   },
+   {
+      name: 'Image 5',
+      link: 'http://www.vogue.com/',
+      img1: '7.jpg',
+      img2: null,
+      img3: null
+   },
+   {
+      name: 'Image 6',
+      link: 'http://www.vogue.com/',
+      img1: '6.jpg',
+      img2: null,
+      img3: null
+   },
+   {
+      name: 'Image 7',
+      link: 'http://www.vogue.com/',
+      img1: '5.jpg',
+      img2: null,
+      img3: null
+   },
+   {
+      name: 'Image 8',
+      link: 'http://www.vogue.com/',
+      img1: '4.jpg',
+      img2: null,
+      img3: null
+   },
+   {
+      name: 'Image 9',
+      link: 'http://www.vogue.com/',
+      img1: '3.jpg',
+      img2: null,
+      img3: null
+   },
+   {
+      name: 'Image 10',
+      link: 'http://www.vogue.com/',
+      img1: '2.jpg',
+      img2: null,
+      img3: null
+   },
+   {
+      name: 'Image 11',
+      link: 'http://www.vogue.com/',
+      img1: '1.jpg',
+      img2: null,
+      img3: null
+   },
+   {
+      name: 'Image 12',
+      link: 'http://www.vogue.com/',
+      img1: '13.jpg',
+      img2: null,
+      img3: null
+   }
+];
+
+console.log($scope.karasPictures);
+
+
+
+
+});
