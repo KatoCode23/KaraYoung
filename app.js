9f4c008e8dcbfcb86223241864ad1304e3679f52
KatoCode23 <Kalen23Imani@gmail.com>
2017-05-01 23:37:32 -0400

    HTML page added and photos on display

--- app.js
+++ app.js
@@ -9,98 +9,77 @@ log('Hello World!!!!')
 
 $scope.karasPictures = [
    {
-      name: 'Home Image',
-      link: 'http://www.vogue.com/',
-      img1: '10.jpg',
-      img2: null,
-      img3: null
-
-   },
-   {
-      name: 'Image 1',
-      link: 'http://www.vogue.com/',
-      img1: '11.jpg',
-      img2: null,
-      img3: null
-
-   },
-   {
-      name: 'Image 2',
-      link: 'http://www.vogue.com/',
-      img1: '12.jpg',
-      img2: null,
-      img3: null
-   },
-   {
-      name: 'Image 3',
-      link: 'http://www.vogue.com/',
-      img1: '9.jpg',
-      img2: null,
-      img3: null
-
-   },
-   {
-      name: 'Image 4',
-      link: 'http://www.vogue.com/',
-      img1: '8.jpg',
-      img2: null,
-      img3: null
-   },
-   {
-      name: 'Image 5',
-      link: 'http://www.vogue.com/',
-      img1: '7.jpg',
-      img2: null,
-      img3: null
-   },
-   {
-      name: 'Image 6',
-      link: 'http://www.vogue.com/',
-      img1: '6.jpg',
-      img2: null,
-      img3: null
-   },
-   {
-      name: 'Image 7',
-      link: 'http://www.vogue.com/',
-      img1: '5.jpg',
-      img2: null,
-      img3: null
-   },
-   {
-      name: 'Image 8',
-      link: 'http://www.vogue.com/',
-      img1: '4.jpg',
-      img2: null,
-      img3: null
-   },
-   {
-      name: 'Image 9',
-      link: 'http://www.vogue.com/',
-      img1: '3.jpg',
-      img2: null,
-      img3: null
-   },
-   {
-      name: 'Image 10',
-      link: 'http://www.vogue.com/',
-      img1: '2.jpg',
-      img2: null,
-      img3: null
-   },
-   {
-      name: 'Image 11',
+      name: 'Beach Photoshoot',
       link: 'http://www.vogue.com/',
       img1: '1.jpg',
-      img2: null,
-      img3: null
+      img2: '2.jpg',
+      img3: '3.jpg'
+
    },
    {
-      name: 'Image 12',
+      name: 'VOGUE',
       link: 'http://www.vogue.com/',
-      img1: '13.jpg',
-      img2: null,
-      img3: null
+      img1: '2.jpg',
+      img2: '3.jpg',
+      img3: '4.jpg'
+
+   },
+   {
+      name: 'ELLE',
+      link: 'http://www.vogue.com/',
+      img1: '3.jpg',
+      img2: '4.jpg',
+      img3: '5.jpg'
+   },
+   {
+      name: 'PLAYBOY',
+      link: 'http://www.vogue.com/',
+      img1: '4.jpg',
+      img2: '5.jpg',
+      img3: '6.jpg'
+
+   },
+   {
+      name: 'PARIS',
+      link: 'http://www.vogue.com/',
+      img1: '5.jpg',
+      img2: '6.jpg',
+      img3: '7.jpg'
+   },
+   {
+      name: 'BEACH COMBER',
+      link: 'http://www.vogue.com/',
+      img1: '6.jpg',
+      img2: '7.jpg',
+      img3: '8.jpg'
+   },
+   {
+      name: 'VALENTINO',
+      link: 'http://www.vogue.com/',
+      img1: '7.jpg',
+      img2: '8.jpg',
+      img3: '9.jpg'
+   },
+   {
+      name: 'VOGUE',
+      link: 'http://www.vogue.com/',
+      img1: '8.jpg',
+      img2: '9.jpg',
+      img3: '10.jpg'
+   },
+   {
+      name: 'COVERGIRL',
+      link: 'http://www.vogue.com/',
+      img1: '9.jpg',
+      img2: '10.jpg',
+      img3: '1.jpg'
+   },
+   {
+      name: 'GLAMOUR',
+      link: 'http://www.vogue.com/',
+      img1: '10.jpg',
+      img2: '1.jpg',
+      img3: '2.jpg'
    }
 ];
 
